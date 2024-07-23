e4c34410bc88852dbeb3e5c72e39cc5ea0e28e51
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-23 09:39:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from submitting the traditional way
+    event.preventDefault();
 
+    // Get the form element by its ID
+    var form = document.getElementById('form1');
+
+    // Retrieve the values of the first name and last name input fields
+    var firstName = form.elements['fname'].value;
+    var lastName = form.elements['lname'].value;
+
+    // Display the concatenated values using alert
+    alert(firstName + " " + lastName);
 }
